24d3e7dc0e79c53a3222a98e91c7a0484ead062d
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-20 23:38:13 -0500

    Added cyclejs

--- src/startApp.js
+++ src/startApp.js
@@ -1,7 +1,22 @@
-export const startApp = function startApp() {
-  console.log("App started");
+import xs from 'xstream';
+import { run } from '@cycle/xstream-run';
+import { makeDOMDriver } from '@cycle/dom';
+import R from 'ramda';
+
+function main(sources) {
+  const sinks = {DOM: null};
+  /**
   var coinbase = web3.eth.coinbase;
   web3.eth.getBalance(coinbase, function (error, balance) {
     console.log(balance.toNumber());
   });
-};
+  **/
+  console.log("App started");
+  return sinks;
+}
+
+export const startApp = function startApp() {
+  run(main, {
+    DOM: makeDOMDriver('#app')
+  });
+}
