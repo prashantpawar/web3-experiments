508625c337d64f6105da87d7c85ae188cfad2231
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-21 15:19:25 -0500

    Working weight slider

--- src/startApp.js
+++ src/startApp.js
@@ -1,22 +1,70 @@
 import xs from 'xstream';
 import { run } from '@cycle/xstream-run';
-import { makeDOMDriver } from '@cycle/dom';
+import { makeDOMDriver, div, input, span } from '@cycle/dom';
 import R from 'ramda';
 
 function main(sources) {
-  const sinks = {DOM: null};
+  const domSource = sources.DOM;
+  const props$ = sources.props;
+
+  const newValue$ = domSource
+    .select('.slider')
+    .events('input')
+    .map(ev => ev.target.value);
+
+  const state$ = props$
+    .map(props => newValue$
+      .map(val => ({
+        label: props.label,
+        unit: props.unit,
+        min: props.min,
+        value: val,
+        max: props.max
+      }))
+      .startWith(props)
+    )
+    .flatten()
+    .remember();
+
+  const vdom$ = state$
+    .map(state =>
+      div('.labeled-slider', [
+        span('.label',
+          state.label + ' ' + state.value + state.unit
+        ),
+        input('.slider', {
+          attrs: {
+            type: 'range',
+            min: state.min,
+            max: state.max,
+            value: state.value
+          }
+        })
+      ])
+    )
+
+  const sinks = {
+    DOM: vdom$,
+    value: state$.map(state => state.value),
+  };
   /**
   var coinbase = web3.eth.coinbase;
   web3.eth.getBalance(coinbase, function (error, balance) {
     console.log(balance.toNumber());
   });
   **/
-  console.log("App started");
   return sinks;
 }
 
 export const startApp = function startApp() {
   run(main, {
-    DOM: makeDOMDriver('#app')
+    DOM: makeDOMDriver('#app'),
+    props: () => xs.of({
+      label: 'Weight',
+      unit: 'kg',
+      value: 70,
+      min: 40,
+      max: 120,
+    })
   });
 }
